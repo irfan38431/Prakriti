69dc6f846dc1773f6e10f334c965481122a44d40
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-09-25 10:40:29 +0530

    Update Title.tsx

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -13,7 +13,7 @@ function Title({ setMessages }: Props) {
     setIsResetting(true);
 
     await axios
-      .get("http://localhost:8000/reset", {
+      .get("https://prakriti-production.up.railway.app/reset", {
         headers: {
           "Content-Type": "application/json",
         },
